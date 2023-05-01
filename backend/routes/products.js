dc7b8c6c9a27e2acf25d0ac4aaf0481c1d63bffb
thevaliantthird <shubh5796@gmail.com>
2023-05-01 10:03:42 +0530

    added own DBMS support for product serach

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,7 @@
 const productsRouter = require("express").Router();
 const pool = require("../db");
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:5796', {reconnect: true});
 
 // get all products
 productsRouter.get("/", async (req, res) => {
@@ -23,5 +25,25 @@ productsRouter.get("/:id", async (req, res) => {
     console.log(error.message);
   }
 });
+
+productsRouter.get("/search/:key", async (req, res) => {
+  try {
+    const { key } = req.params;
+    socket.on('connect', function (socket) {
+        console.log('Connected!');
+        socket.emit('CH01', 'me', 'test msg');
+        socket.on('message', (data) => {
+          res.status(200).json(data.split(' '))
+      })
+    });
+    
+    
+    
+    res.status(200).json(product.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 
 module.exports = productsRouter;
